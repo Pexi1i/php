c77ed633f8205d69c89b61ba08c320c849e83683
Pexi1i <zaxcev9898@gmail.com>
2025-11-27 14:56:44 +0100

    d

---
+++ php/php2/formularz.php
@@ -0,0 +1,28 @@
+welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"];?>
+
+<?php
+//Połączenie z serwerem my_SQL
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "goetel";
+$conn = mysqli_connect($servername,$username,$password,$database);
+if(!conn){
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+$name= $_POST["name"];
+$email= $_POST["email"];
+$sql = "INSERT INTO MyGuests (firstname, email) values ('$name','$email')";
+echo $sql;
+if(mysqli_query($conn,$sql)){
+    echo "NEW record created successfully";
+} else {
+    echo "Error:". $sql . "<br>" . mysqli_error($conn);
+} 
+
+
+
+?>
